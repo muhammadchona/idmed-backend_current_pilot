5c748055b3965fbde2667fe46b270397023c101f
muhammad <muhammadchona@gmail.com>
2021-10-21 21:31:37 +0200

    mudancas na classe drug e regime therapeutic

--- grails-app/domain/mz/org/fgh/sifmoz/backend/drug/Drug.groovy
+++ grails-app/domain/mz/org/fgh/sifmoz/backend/drug/Drug.groovy
@@ -1,20 +1,24 @@
 package mz.org.fgh.sifmoz.backend.drug
 
 import grails.rest.Resource
+import mz.org.fgh.sifmoz.backend.clinicSector.ClinicSector
 import mz.org.fgh.sifmoz.backend.doctor.Doctor
 import mz.org.fgh.sifmoz.backend.episode.Episode
 import mz.org.fgh.sifmoz.backend.form.Form
+import mz.org.fgh.sifmoz.backend.therapeuticRegimen.TherapeuticRegimen
 
 class Drug {
     String id
     int packSize
-    boolean sideTreatment
+ //   boolean sideTreatment
     String name
-    double defaultTament
-    int defaultTimes
+    double defaultTreatment // numero de toma
+    int defaultTimes// numero de vezes a tomar
+    String defaultPeriodTreatment //  periofo a tomar --commbo (dia , semana , mes, ano)
+    String fnmCode
     String uuidOpenmrs = UUID.randomUUID().toString()
     static belongsTo = [form: Form]
-
+  //  static hasMany = [therapeuticRegimen: TherapeuticRegimen]
     static mapping = {
         id generator: "uuid"
     }
@@ -22,7 +26,7 @@ class Drug {
     static constraints = {
         name nullable: false, blank: false
         packSize(min: 0)
-        defaultTament(min: 0)
+     //   defaultTreatment(min: 1.00)
         defaultTimes(min:1)
     }
 }
--- grails-app/domain/mz/org/fgh/sifmoz/backend/regimenDrug/RegimenDrug.groovy
+++ grails-app/domain/mz/org/fgh/sifmoz/backend/regimenDrug/RegimenDrug.groovy
@@ -1,11 +1,17 @@
 package mz.org.fgh.sifmoz.backend.regimenDrug
 
+import mz.org.fgh.sifmoz.backend.drug.Drug
+import mz.org.fgh.sifmoz.backend.therapeuticRegimen.TherapeuticRegimen
+
 class RegimenDrug {
     String id
-    double amPerTime
-    int timesPerDay
-    boolean modified
-    String notes
+  //  double amPerTime
+   // int timesPerDay
+   // boolean modified
+   // String notes
+    // static belongsTo = [drug: Drug,therapeuticRegimen:TherapeuticRegimen]
+ //   Drug drug
+  //  TherapeuticRegimen therapeuticRegimen
 
     static mapping = {
         id generator: "uuid"
