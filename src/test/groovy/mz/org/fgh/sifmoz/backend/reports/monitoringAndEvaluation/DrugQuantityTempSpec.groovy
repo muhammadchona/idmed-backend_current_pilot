dcab79659688f922028ae0143e78d71dd2c80ead
miliate <erciliomiliate@gmail.com>
2022-05-18 11:59:49 +0200

    Processamento de report Modulo de STOCK e modulo de monitoria e avaliação

---
+++ src/test/groovy/mz/org/fgh/sifmoz/backend/reports/monitoringAndEvaluation/DrugQuantityTempSpec.groovy
@@ -0,0 +1,18 @@
+package mz.org.fgh.sifmoz.backend.reports.monitoringAndEvaluation
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class DrugQuantityTempSpec extends Specification implements DomainUnitTest<DrugQuantityTemp> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
