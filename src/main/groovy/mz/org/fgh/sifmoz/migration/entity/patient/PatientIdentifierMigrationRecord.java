02a3ff24e96edfadaf9ef82f122756416ff36811
miliate <erciliomiliate@gmail.com>
2022-07-26 11:58:02 +0200

    criacao das classes que  que mapeiam as entidades do IDART, (pacientes, dispensas)

---
+++ src/main/groovy/mz/org/fgh/sifmoz/migration/entity/patient/PatientIdentifierMigrationRecord.java
@@ -0,0 +1,82 @@
+package mz.org.fgh.sifmoz.migration.entity.patient;
+
+import mz.org.fgh.sifmoz.migration.base.log.AbstractMigrationLog;
+import mz.org.fgh.sifmoz.migration.base.record.AbstractMigrationRecord;
+import mz.org.fgh.sifmoz.migration.base.record.MigrationRecord;
+
+import java.util.List;
+
+public class PatientIdentifierMigrationRecord extends AbstractMigrationRecord {
+
+
+    private Integer id;
+
+    private PatientMigrationRecord patient;
+
+    private String value;
+
+    private IdentifierTypeMigrationRecord type;
+
+    private String valueEdit;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public PatientMigrationRecord getPatient() {
+        return patient;
+    }
+
+    public void setPatient(PatientMigrationRecord patient) {
+        this.patient = patient;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public IdentifierTypeMigrationRecord getType() {
+        return type;
+    }
+
+    public void setType(IdentifierTypeMigrationRecord type) {
+        this.type = type;
+    }
+
+    public String getValueEdit() {
+        return valueEdit;
+    }
+
+    public void setValueEdit(String valueEdit) {
+        this.valueEdit = valueEdit;
+    }
+
+    @Override
+    public List<AbstractMigrationLog> migrate() {
+        return null;
+    }
+
+    @Override
+    public void updateIDMEDInfo() {
+
+    }
+
+    @Override
+    public long getId() {
+        return 0;
+    }
+
+    @Override
+    public String getEntityName() {
+        return null;
+    }
+
+    @Override
+    public void generateUnknowMigrationLog(MigrationRecord record, String message) {
+
+    }
+}
---
+++ src/main/groovy/mz/org/fgh/sifmoz/migration/entity/prescription/RegimeTerapeuticoMigrationRecord.java
@@ -0,0 +1,99 @@
+package mz.org.fgh.sifmoz.migration.entity.prescription;
+
+import mz.org.fgh.sifmoz.migration.base.log.AbstractMigrationLog;
+import mz.org.fgh.sifmoz.migration.base.record.AbstractMigrationRecord;
+import mz.org.fgh.sifmoz.migration.base.record.MigrationRecord;
+
+import java.util.List;
+
+public class RegimeTerapeuticoMigrationRecord extends AbstractMigrationRecord {
+    private Integer regimeid;
+    private String regimeesquema;
+    private boolean active;
+    private String regimenomeespecificado;
+    private String codigoregime;
+    private String tipoDoenca;
+    private String regimeesquemaidart;
+
+    public Integer getRegimeid() {
+        return regimeid;
+    }
+
+    public void setRegimeid(Integer regimeid) {
+        this.regimeid = regimeid;
+    }
+
+    public String getRegimeesquema() {
+        return regimeesquema;
+    }
+
+    public void setRegimeesquema(String regimeesquema) {
+        this.regimeesquema = regimeesquema;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRegimenomeespecificado() {
+        return regimenomeespecificado;
+    }
+
+    public void setRegimenomeespecificado(String regimenomeespecificado) {
+        this.regimenomeespecificado = regimenomeespecificado;
+    }
+
+    public String getCodigoregime() {
+        return codigoregime;
+    }
+
+    public void setCodigoregime(String codigoregime) {
+        this.codigoregime = codigoregime;
+    }
+
+    public String getTipoDoenca() {
+        return tipoDoenca;
+    }
+
+    public void setTipoDoenca(String tipoDoenca) {
+        this.tipoDoenca = tipoDoenca;
+    }
+
+    public String getRegimeesquemaidart() {
+        return regimeesquemaidart;
+    }
+
+    public void setRegimeesquemaidart(String regimeesquemaidart) {
+        this.regimeesquemaidart = regimeesquemaidart;
+    }
+
+    @Override
+    public List<AbstractMigrationLog> migrate() {
+        return null;
+    }
+
+    @Override
+    public void updateIDMEDInfo() {
+
+    }
+
+    @Override
+    public long getId() {
+        return 0;
+    }
+
+    @Override
+    public String getEntityName() {
+        return null;
+    }
+
+    @Override
+    public void generateUnknowMigrationLog(MigrationRecord record, String message) {
+
+    }
+
+}
