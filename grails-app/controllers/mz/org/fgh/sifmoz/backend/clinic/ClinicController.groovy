ecf727b5a544e7e9436ce1778ddda6e3daab5327
Joao Nuno Mabjaia <94103183+joaonunomabjaia@users.noreply.github.com>
2022-09-06 10:03:47 +0200

    Clinic call [Patient registation Form]

--- grails-app/controllers/mz/org/fgh/sifmoz/backend/clinic/ClinicController.groovy
+++ grails-app/controllers/mz/org/fgh/sifmoz/backend/clinic/ClinicController.groovy
@@ -32,7 +32,7 @@ class ClinicController extends RestfulController{
     }
 
     def show(String id) {
-        render JSONSerializer.setJsonObjectResponse(clinicService.(id)) as JSON
+        render JSONSerializer.setJsonObjectResponse(clinicService.get(id)) as JSON
     }
 
     @Transactional
